f2868b22705f8c36378bd22a3c0bc4f4a7bb7be4
kemalhanan <muhammad.kemal13@ui.ac.id>
2023-02-22 13:35:12 +0700

    Tutorial 2

--- django_tutorial/money_tracker/urls.py
+++ django_tutorial/money_tracker/urls.py
@@ -3,6 +3,7 @@ from money_tracker.views import show_tracker
 from money_tracker.views import show_xml
 from money_tracker.views import show_json
 from money_tracker.views import show_xml_by_id, show_json_by_id
+from money_tracker.views import create_transaction
 
 app_name = 'money_tracker'
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), #sesuaikan dengan nama fungsi yang dibuat
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('create', create_transaction, name='create_transaction'),
 ]
--- django_tutorial/money_tracker/views.py
+++ django_tutorial/money_tracker/views.py
@@ -3,15 +3,28 @@ from django.shortcuts import render
 from money_tracker.models import TransactionRecord
 from django.http import HttpResponse
 from django.core import serializers
+from django.http import HttpResponseRedirect
+from money_tracker.forms import TransactionRecordForm
+from django.urls import reverse
 
 # Create your views here.
 def show_tracker(request):
     transaction_data = TransactionRecord.objects.all()
     context = {
     'list_of_transactions': transaction_data,
-    'name': 'Kak Athal'
+    'name': 'Kemal'
     }
     return render(request, "tracker.html", context)
+
+def create_transaction(request):
+    form = TransactionRecordForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('money_tracker:show_tracker'))
+
+    context = {'form': form}
+    return render(request, "create_transaction.html", context)
 
 def show_xml(request):
     data = TransactionRecord.objects.all()
